cbb542eab87d0e95e42d0f3d7a4ce3649f5bf8fd
Ubuntu <ubuntu@ip-172-31-17-38.us-east-2.compute.internal>
2025-02-07 23:53:57 +0000

    Program from the auction

---
+++ task.py
@@ -0,0 +1,10 @@
+my_empty_dictionary = {}
+
+my_empty_dictionary["first"] = "Hello"
+
+if "Hello" in my_empty_dictionary:
+    print(my_empty_dictionary)
+
+values = {'a': 134, 'b' : 125}
+
+print(max(values))
